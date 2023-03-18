14f6674672badb24bf1f343f78043e0624a7b844
lukaTarimana <104419299+lukaTarimana@users.noreply.github.com>
2023-03-18 12:29:55 +0400

    feat: layout 101

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import Home from "./pages/home/Home";
-// import Layout from "./components/layout/Layout";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import Layout from "./components/layout/Layout";
+import "./styles.css";
 
 const App = () => {
+  const { chains, provider } = configureChains(
+    [mainnet, polygon, optimism, arbitrum],
+    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "My RainbowKit App",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: false,
+    connectors,
+    provider,
+  });
+
   return (
-    <Router>
-      {/* A <Routes> looks through its children <Route>s and
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Router>
+          {/* A <Routes> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-      <Routes>
-        <Route path="/about">{/* <About /> */}</Route>
-        <Route path="/users">{/* <Users /> */}</Route>
-        <Route path="/"></Route>
-      </Routes>
-    </Router>
+          <Layout>
+            <Routes>
+              <Route path="/movies">{/* <Movies /> */}</Route>
+              <Route path="/streaming">{/* <Streaming /> */}</Route>
+              <Route path="/videos">{/* <Videos /> */}</Route>
+              <Route path="/">{/*<Home/> */}</Route>
+            </Routes>
+          </Layout>
+        </Router>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 };
 
