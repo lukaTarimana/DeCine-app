e7b2e94fd9a9abcc110cb705a3e5916393709b9a
lukaTarimana <104419299+lukaTarimana@users.noreply.github.com>
2023-03-18 15:51:49 +0400

    refactor: create video

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -83,7 +83,7 @@ const Layout = ({ children }) => {
               Sign Up
             </Button>
           </Navbar.Item> */}
-          <ConnectButton />
+          <ConnectButton chainStatus={"none"} showBalance={false} />
         </Navbar.Content>
         <Navbar.Collapse
           css={{
@@ -91,6 +91,7 @@ const Layout = ({ children }) => {
             position: "absolute",
             backgroundColor: "#000",
             overflowY: "none",
+            height: isNavbarVisible && "calc(100vh - 76px)",
           }}
           isOpen={isNavbarVisible}
         >
--- src/pages/create-video/CreateVideo.jsx
+++ src/pages/create-video/CreateVideo.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import {
+  Card,
+  Spacer,
+  Button,
+  Text,
+  Input,
+  Row,
+  Checkbox,
+  Container,
+} from "@nextui-org/react";
 
 const CreateVideo = () => {
-  return <div>CreateVideo</div>;
+  const handleSubmit = () => {
+    console.log("submited");
+  };
+  return (
+    <div>
+      <Container
+        display="flex"
+        alignItems="center"
+        justify="center"
+        css={{ minHeight: "100vh", w: "100%" }}
+      >
+        <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+          <Card css={{ mw: "100%", p: "20px" }}>
+            <Text
+              size={24}
+              weight="bold"
+              css={{
+                as: "center",
+                mb: "20px",
+              }}
+            >
+              NextUI Login
+            </Text>
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Title"
+            />
+            <Spacer y={1} />
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Description"
+            />
+            <Spacer y={1} />
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Video link"
+            />
+            <Spacer y={1} />
+            <Input
+              clearable
+              bordered
+              fullWidth
+              color="primary"
+              size="lg"
+              placeholder="Thumbnail"
+            />
+            <Row justify="space-between">
+              <Checkbox>
+                <Text size={14}>+18 content</Text>
+              </Checkbox>
+            </Row>
+            <Spacer y={1} />
+            <Button type="submit">Upload Video</Button>
+          </Card>
+        </form>
+      </Container>
+    </div>
+  );
 };
-
 export default CreateVideo;
