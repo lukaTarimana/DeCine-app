b47db57fa06b335fac5704bc0644e961f753e9a2
lukaTarimana <104419299+lukaTarimana@users.noreply.github.com>
2023-03-18 13:01:16 +0400

    refactor: home page

--- src/components/videos/Video.jsx
+++ src/components/videos/Video.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import classes from "../../modules/Video.module.css";
-import { Card, Image, Avatar } from "@nextui-org/react";
+import { Card, Link, Image, Avatar } from "@nextui-org/react";
 
 const Video = (props) => {
   return (
-    <Card>
-      <Card.Header css={{width:"unset"}}>
-        <Image
-          width={320}
-          height={180}
-          css={{borderRadius: "1.5rem"}}
-          src={props.thumbnail}
-          alt={"DeCine Video Thumbnail"}
-          objectFit={"cover"}
-        />
-      </Card.Header>
-      <Card.Body>
-          <div className={classes['video-info']}>
-                <Avatar src={props.avatar}  size="md" />
-                <div className={classes['video-description']}>
-                    <strong>{props.title}</strong>
-                    <span>{props.username}</span>
-                    <span>{props.views} views · {props.date}</span>
-                </div>
+    <Link href={`/videos/${props.id}`}>
+      <Card>
+        <Card.Header css={{ width: "unset" }}>
+          <Image
+            width={320}
+            height={180}
+            css={{ borderRadius: "1.5rem" }}
+            src={props.thumbnail}
+            alt={"DeCine Video Thumbnail"}
+            objectFit={"cover"}
+          />
+        </Card.Header>
+        <Card.Body>
+          <div className={classes["video-info"]}>
+            <Avatar src={props.avatar} size="md" />
+            <div className={classes["video-description"]}>
+              <strong>{props.title}</strong>
+              <span>{props.username}</span>
+              <span>
+                {props.views} views · {props.date}
+              </span>
+            </div>
           </div>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 export default Video;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,17 +6,20 @@ const Home = (props) => {
   return (
     <Grid.Container justify="center" gap={1}>
       {props.items.map((item, i) => {
-        return <Grid xl={2} lg={3} md={4} sm={6}>
-          <Video
-            key={i}
-            thumbnail={item.thumbnail}
-            avatar={item.avatar}
-            title={item.title}
-            username={item.username}
-            views={item.views}
-            date={item.date}
-          />
-        </Grid>;
+        return (
+          <Grid xl={2} lg={3} md={4} sm={6}>
+            <Video
+              key={i}
+              thumbnail={item.thumbnail}
+              avatar={item.avatar}
+              title={item.title}
+              username={item.username}
+              views={item.views}
+              date={item.date}
+              id={i}
+            />
+          </Grid>
+        );
       })}
     </Grid.Container>
   );
